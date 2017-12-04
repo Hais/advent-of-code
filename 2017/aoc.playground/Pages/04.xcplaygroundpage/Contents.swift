f0cdae0a746ab9fe2d381b5e6334387033acb3c8
Hais Deakin <hais@weshop.co.uk>
2017-12-04 08:23:08 +0000

    Day 04 tidy up

--- 2017/aoc.playground/Pages/04.xcplaygroundpage/Contents.swift
+++ 2017/aoc.playground/Pages/04.xcplaygroundpage/Contents.swift
@@ -7,6 +7,8 @@ import XCTest
 let p1 = { (i: [String]) -> Bool in i.count == Set<String>(i).count }
 let p2 = { (i: [String]) -> Bool in p1(i.map { $0.characters.sorted() }.map{ "\($0)" }) }
 
+let countValid: ([[String]], ([String]) -> Bool) -> Int = { arr, fn in arr.map(fn).map{ $0 ? 1 : 0 }.reduce(0, +) }
+
 // read input file
 let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
 let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
@@ -17,9 +19,7 @@ XCTAssertTrue(p1(["aa", "bb", "cc", "dd", "ee"]))
 XCTAssertFalse(p1(["aa", "bb", "cc", "dd", "aa"]))
 XCTAssertTrue(p1(["aa", "bb", "cc", "dd", "aaa"]))
 
-let p1Answer = words.map(p1).map { $0 ? 1 : 0 }.reduce(0, +)
-
-print("Part 1 is", p1Answer)
+print("Part 1 is", countValid(words, p1))
 
 XCTAssertTrue(p2(["abcde", "fghij"]))
 XCTAssertFalse(p2(["abcde", "xyz", "ecdab"]))
@@ -27,8 +27,6 @@ XCTAssertTrue(p2(["a", "ab", "abc", "abd", "abf", "abj"]))
 XCTAssertTrue(p2(["iiii", "oiii", "ooii", "oooi", "oooo"]))
 XCTAssertFalse(p2(["oiii", "ioii", "iioi", "iiio", "is"]))
 
-let p2Answer = words.map(p2).map { $0 ? 1 : 0 }.reduce(0, +)
-
-print("Part 2 is", p2Answer)
+print("Part 2 is", countValid(words, p2))
 
 //: [Next](@next)
